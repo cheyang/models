b346cb1cd3bccc89851e147a74dc4674340b566d
cheyang <cheyang@163.com>
2018-09-26 17:13:48 +0800

    create default dir

--- dist_mnist_estimator.sh
+++ dist_mnist_estimator.sh
@@ -5,6 +5,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export PYTHONPATH=$PYTHONPATH:$DIR
 
 cd $DIR
+mkdir -p /root/.pip/
 cp pip.conf /root/.pip/pip.conf
 pip install -r official/requirements.txt
 cd official/mnist
